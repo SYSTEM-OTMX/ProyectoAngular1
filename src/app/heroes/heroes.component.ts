84aeb8f3d25c707fb237aecbf0d4a57dbc21be3b
SYSTEM-OTMX <gtainside222@gmail.com>
2021-03-23 13:44:30 -0600

    VERCION 3 PRACTICA

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,25 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero} from '../hero';
+import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
+
+
 
 @Component({
+
   selector: 'app-heroes',
+
   templateUrl: './heroes.component.html',
+
   styleUrls: ['./heroes.component.css']
+
 })
+
 export class HeroesComponent implements OnInit {
-  /*crear clase*/
-  hero: Hero={
-    id:1,
-    name:'IRON MAN',
-    edad: 40,
-    superpoder:'EL DINERO',
-    universo:'MARVEL',
-    identidad:'TONY STARK'
-  };
+
+
+
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+
+
+
 
   constructor() { }
 
+
+
   ngOnInit(): void {
+
   }
+
+  onSelect(hero: Hero): void {
+
+    this.selectedHero = hero;
+
+  }
+
+
 
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from"./hero";
+
+export const HEROES: Hero[]=[
+    {id:11,name:"superman",edad:50,superpoder:"extraterrestre",universo:"DC",identidad:"Sysntax.error"},
+    {id:12,name:"batman",edad:40,superpoder:"extraterrestre",universo:"DC",identidad:"Sysntax.error"},
+    {id:13,name:"hulk",edad:40,superpoder:"extraterrestre",universo:"MARVEL",identidad:"Sysntax.error"},
+    {id:14,name:"iron man",edad:40,superpoder:"extraterrestre",universo:"MARVEL",identidad:"Sysntax.error"},
+    {id:13,name:"thor",edad:400,superpoder:"extraterrestre",universo:"MARVEL",identidad:"Sysntax.error"},
+    {id:13,name:"spiderman",edad:20,superpoder:"extraterrestre",universo:"MARVEL",identidad:"Sysntax.error"},
+    {id:13,name:"black widow",edad:30,superpoder:"extraterrestre",universo:"MARVEL",identidad:"Sysntax.error"},
+    {id:13,name:"flash",edad:25,superpoder:"extraterrestre",universo:"DC",identidad:"Sysntax.error"},
+    {id:13,name:"bruja escarlata",edad:35,superpoder:"extraterrestre",universo:"MARVEL",identidad:"Sysntax.error"},
+    {id:13,name:"mujer maravilla",edad:40,superpoder:"extraterrestre",universo:"DC",identidad:"Sysntax.error"}
+];
